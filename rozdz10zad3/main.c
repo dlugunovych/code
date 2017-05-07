01a841691e29d7370f17c02139fcef3e0a4a3e2b
DESKTOP-ST0N8QN\Kateryna <kate.dlugunovych@gmail.com>
2017-05-07 22:33:52 +0200

    ccc

--- rozdz10zad3/main.c
+++ rozdz10zad3/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+int BiggestValue (int tArray[], int dSizeOfArray);
 
 int main()
 {
-    printf("Hello world!\n");
+    int tSourceArray[5] = {1,20,3,9,2};
+    printf("%d", BiggestValue(tSourceArray, 5));
     return 0;
+}
+
+int BiggestValue (int tArray[], int dSizeOfArray)
+{
+   int dBiggestDigit = 0;
+   for (int i = 0; i < dSizeOfArray; i++)
+   {
+      if (tArray[i]>dBiggestDigit)
+      {
+         dBiggestDigit = tArray[i];
+      }
+   }
+   return dBiggestDigit;
 }
